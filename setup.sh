b56eae7698182273488e6ed4bec94cb49c8bea85
kurochan <kuro@kurochan.org>
2013-10-29 19:40:13 +0900

    add setup script

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+if [ "$1" = "" ]; then
+  arch=mac
+else
+  arch=$1
+fi
+
+echo "link .vim"
+ln -s `pwd`/vim/dot.vim ~/.vim
+echo "link .vimrc"
+ln -s `pwd`/vim/dot.vimrc ~/.vimrc
+echo "install neobundle"
+mkdir -p ~/.vim/bundle
+git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+
+echo "link .zshrc"
+ln -s `pwd`/zsh/dot.zshrc ~/.zshrc
+
+echo "link .gitconfig"
+ln -s `pwd`/git/dot.gitconfig ~/.gitconfig
+
